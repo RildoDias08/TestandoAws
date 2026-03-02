3c2c80eb1a14c77d4f8bf9a50133fdc025af7d8e
Rildo Dias <rildodias08@gmail.com>
2026-03-01 22:25:42 -0300

    esp

--- s3.sh
+++
@@ -1,7 +0,0 @@
-function envio_s3(){
-	echo "Iniciando envio para o S3..."
-	echo ""
-	aws s3 sync ./client/dist/ s3://front-rio-aws --profile bia --delete
-	echo "Envio finalizado com sucesso!"
-}
-envio_s3
