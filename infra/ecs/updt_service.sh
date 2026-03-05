f44f7adfc59d7425206da758020e5e3637e793d6
Rildo Dias <rildodias08@gmail.com>
2026-03-05 19:43:41 -0300

    corrigindo erro de att task, task_arn antigo ditava regra

--- infra/ecs/updt_service.sh
+++ infra/ecs/updt_service.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-read -rp "Nome do ECS Cluster (ex: rio-cluster): " CLUSTER
 
+read -rp "Nome do ECS Cluster (ex: rio-cluster): " CLUSTER
 read -rp "Nome do ECS Service (ex: rio-service): " SERVICE
+read -rp "Task family (ex: rio-backend): " FAMILY
+read -rp "Desired count (ex: 2): " DESIRED
+
+LATEST_TD_ARN="$(
+  aws ecs list-task-definitions \
+    --region "$AWS_REGION" \
+    --family-prefix "$FAMILY" \
+    --status ACTIVE \
+    --sort DESC \
+    --max-results 1 \
+    --query "taskDefinitionArns[0]" \
+    --output text
+)"
+
+echo "[INFO] Usando latest taskdef: $LATEST_TD_ARN"
 
 aws ecs update-service \
+  --region "$AWS_REGION" \
   --cluster "$CLUSTER" \
   --service "$SERVICE" \
-  --force-new-deployment --desired-count 2
+  --task-definition "$LATEST_TD_ARN" \
+  --desired-count "$DESIRED" \
+  --force-new-deployment
+
 
 aws ecs wait services-stable \
   --cluster "$CLUSTER" \
