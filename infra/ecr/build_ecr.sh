e5f10454c91aec5c6e6bdd00e55ff3fdcae02cf5
Rildo Dias <rildodias08@gmail.com>
2026-03-05 01:59:45 -0300

    correção estetica

--- infra/ecr/build_ecr.sh
+++ infra/ecr/build_ecr.sh
@@ -7,13 +7,15 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 ECR_REGISTRY="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
 read -rp "Nome do repo (ex: bia): " REPO
+read -rp "Nome da iamgem (ex: bia): " NAME_IMG
+
 
 echo "🔐 Login no ECR..."
 aws ecr get-login-password --region "$AWS_REGION" \
 | docker login --username AWS --password-stdin "$ECR_REGISTRY"
 
 echo "🏗️ Build..."
-docker build -t "$REPO" .
+docker build -t "$NAME_IMG" .
 
 echo "🏷️ Tag..."
 docker tag "$REPO:latest" "$ECR_REGISTRY/$REPO:latest"
